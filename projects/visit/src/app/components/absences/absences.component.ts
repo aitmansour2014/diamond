299387b63fbf02563f27b954286961fbaa2baef0
aitmansour2014 <35490310+aitmansour2014@users.noreply.github.com>
2024-03-07 12:00:34 +0000

    Update Absence Ccmponenet

--- projects/visit/src/app/components/absences/absences.component.ts
+++ projects/visit/src/app/components/absences/absences.component.ts
@@ -1,97 +1,130 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Absences } from '../../model/absences.model';
 import { NgFor, NgIf } from '@angular/common';
 import { SharedModuleModule } from '../../shared-module/shared-module.module';
 import dayGridPlugin from '@fullcalendar/daygrid'; // Importez le plugin dayGrid
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { Calendar } from '@fullcalendar/core'
+import { DatePipe } from '@angular/common';
+import { CheckAvailibilityHostessService } from '../../services/check-availibility-hostess.service';
 
+interface Categorie1 {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-absences',
   standalone: true,
   imports: [NgIf, NgFor, SharedModuleModule,FullCalendarModule],
   templateUrl: './absences.component.html',
+  providers: [DatePipe],
   styleUrl: './absences.component.css'
 })
-export class AbsencesComponent {
+export class AbsencesComponent implements OnInit{
   //disponibilites: any[] = [];
   plugins = [dayGridPlugin];
 
  // Liste des plugins à utiliser
+ categorieAbbsence: Categorie1[] = [
+  {value: 'MALADIE', viewValue: 'Maladie'},
+  {value: 'VACANCE', viewValue: 'Vacance'},
+  {value: 'TRAVAIL', viewValue: 'Travail'},
+  {value: 'ÉVÉNEMENT FAMILIAL', viewValue: 'Événement Familial'},
+];
+
   absences: Absences[] = [{ 
     id: 1,
     idHostesse: 1,
-    dateStart: "Wed Mar 06 2024 15:48:59 GMT+0100 (UTC+01:00)",
-    dateEnd: "Wed Mar 07 2024 15:48:59 GMT+0100 (UTC+01:00)",
+    dateStart: "2024-03-06T15:48:59",
+    dateEnd: "2024-03-07T15:48:59",
     category: " ",
-    reason: " " 
+    reason: "Salam Aleicom " 
   },
   { 
     id: 2,
     idHostesse: 1,
-    dateStart: "Wed Mar 08 2024 15:48:59 GMT+0100 (UTC+01:00)",
-    dateEnd: "Wed Mar 09 2024 15:48:59 GMT+0100 (UTC+01:00)",
+    dateStart: "2024-03-08T15:48:59",
+    dateEnd: "2024-03-09T15:48:59",
     category: " ",
-    reason: " " 
+    reason: "aaa " 
   },
   { 
     id:3,
     idHostesse: 1,
-    dateStart: "Wed Mar 10 2024 15:48:59 GMT+0100 (UTC+01:00)",
-    dateEnd: "Wed Mar 11 2024 15:48:59 GMT+0100 (UTC+01:00)",
+    dateStart: "2024-03-10T15:48:59",
+    dateEnd: "2024-03-11T15:48:59",
     category: " ",
-    reason: " " 
+    reason: " bbbb" 
   }];
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private datePipe: DatePipe, private checkAvailibilityHostessService: CheckAvailibilityHostessService) {
     
   }
-  ngAfterViewInit() {
+  ngOnInit(): void {
+    this. calendrier()
+   // throw new Error('Method not implemented.');
+  }
+  calendrier() {
     const calendarEl: HTMLElement = this.elementRef.nativeElement.querySelector('#calendar');
-
+    console.log(this.absences)
+    const events = this.convertirDisponibilitesEnEvenements(this.absences)
+    console.log(events)
     const calendar = new Calendar(calendarEl, {
       plugins: [dayGridPlugin],
       initialView: 'dayGridMonth',
       weekends: false,
-      events: [
-        { title: 'Meeting', start: new Date() },
-        { title: 'Meeting1', start: new Date('2024-03-10T15:48:59'), end: new Date('2024-03-11T15:48:59') },
-        { title: 'Meeting2', start: new Date('2024-03-12T15:48:59'), end: new Date('2024-03-13T15:48:59') }
-      ]
+      events:events
     });
 
     calendar.render();
 
   }
-  ajouterAbsence(dateDebut: string, dateFin: string) {
+  ajouterAbsence(dateDebut: string, dateFin: string , raison: string , categorie: string) {
     this.absences.push({ 
       id: 0,
       idHostesse: 0,
       dateStart: dateDebut,
       dateEnd: dateFin,
-      category: " ",
-      reason: " " 
+      category: categorie,
+      reason: raison 
     });
-    console.log(this.absences)
-    console.log(new Date())
+    this. calendrier()
     
+  }
+  convertDateFormat(originalDate: string) {
+    const parsedDate = new Date(originalDate);
+    const formattedDate = this.datePipe.transform(parsedDate, 'dd/MM/yyyy HH:mm');
+   // console.log(formattedDate); // Output: 2024-03-11T15:48:59
+    return formattedDate
   }
 
   supprimerAbsence(index: number) {
     this.absences.splice(index, 1);
+    this. calendrier()
+    this.checkAvailability()
   }
    // Méthode pour convertir les disponibilités en format d'événement attendu par FullCalendar
    convertirDisponibilitesEnEvenements(absences: Absences[]): any[] {
     return absences.map(absence => ({
-      title: absence.reason,
-      start: absence.dateStart,
-      end: absence.dateEnd
-    }));
-  }
+      title: absence.category+" : " + absence.reason,
+      start:  absence.dateStart,
+      end:  absence.dateEnd
+   }));  }
 
   // Suppose que disponibilites est mis à jour dès qu'une nouvelle disponibilité est ajoutée ou supprimée
-  ngOnChanges() {
-    this.absences = this.convertirDisponibilitesEnEvenements(this.absences);
+  // ngOnChanges() {
+  //   this.absences = this.convertirDisponibilitesEnEvenements(this.absences);
+  // }
+  checkAvailability() {
+    const dateStart = "2024-03-08T15:48:59";
+    const dateEnd = "2024-03-10T15:48:59";
+    const isAvailable = this.checkAvailibilityHostessService.isHostessAvailable(dateStart, dateEnd);
+    
+    if (isAvailable) {
+      console.log('Hostess is available within the specified period.');
+    } else {
+      console.log('Hostess is not available within the specified period.');
+    }
   }
 }
